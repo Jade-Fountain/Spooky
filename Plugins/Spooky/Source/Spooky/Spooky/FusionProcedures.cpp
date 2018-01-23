3f90d3875f4c9a025c96ccc8f2fa561c4164a48b
Jake Fountain <jake.fountain@uon.edu.au>
2018-01-24 01:24:59 +1100

    implemented get required parents. commented out the continuous version because its probably more trouble than its worth

--- Plugins/Spooky/Source/Spooky/Spooky/FusionProcedures.cpp
+++ Plugins/Spooky/Source/Spooky/Spooky/FusionProcedures.cpp
@@ -20,34 +20,80 @@
 
 namespace spooky{
 
-    Vector3f ArticulatedModel::getPositionFlex(){
+    //TODO: try this?
+    // Node::getRequiredParentsCts(const Measurement::Ptr& m, const Transform3D& toFusionSpace){
+    //         Eigen::Vector<float,6> error = toFusionSpace * m->getPosRot() - getGlobalPose().toPosRot();
+            
+    //         //Iterate through parents until enough flex is found
+    //         std::vector<Node::Ptr> parents;
+    //         parents.push_back(std::make_shared<Node>(this));
+    //         Eigen::Matrix<float,6> varTotal = Eigen::Vector3f::Zero();
+    //         Transform3D pose = Transform3D::Identity();
+    //         float flex = 0;
+    //         const float flex_threshold = 0.9;
+    //         while(true){
+    //             //Assume decoupling between nodes
+    //             pose = parents.back()->getLocalPose() * pose;
+    //             varTotal = parents.back()->getLocalPoseVariance(pose,varTotal);
+    //             flex = std::exp(-error.transpose()*varTotal*error)
+    //             if(flex<flex_threshold){
+    //                 //We dont need anymore nodes to fuse data
+    //                 break;
+    //             } else {
+    //                 //We need more nodes
+    //                 parents.push_back(parents.back()->parent);
+    //             }
+    //         }
+    //         return parents;
+    // }
 
-        for(int i = 0; i < articulations.size(); i++){
-            variance = state->articulation[i].variance;
-        }
-    }
-
-    void ArticulatedModel::fusePositionMeasurement(const Measurement::Ptr& m, const Transform3D& toFusionSpace){
-        //Calculate error
-        Eigen::Vector3f error = toFusionSpace * m->getPosition() - getGlobalPose().translation();
-        
+    std::vector<Node::Ptr> Node::getRequiredParents(const Measurement::Ptr& m){
         //Iterate through parents until enough flex is found
-        Node::Ptr n = std::make_shared<Node>(this);
-        Eigen::Vector3f flex = Eigen::Vector3f::Zero();
-        while(flex < error.norm()){
-            flex += n->getPositionFlex();
+        std::vector<Node::Ptr> parents;
+        parents.push_back(std::make_shared<Node>(this));
+        //Positional degrees of freedom
+        int p_dof = 0;
+        //rotational degrees of freedom
+        int r_dof = 0;
+        //Required dof:
+        int p_dof_req = m->getRequiredPDoF();
+        int r_dof_req = m->getRequiredRDoF();
+
+        const float flex_threshold = 0.9;
+        bool hasLeverChild = false;
+        while(true){
+            //Assume decoupling between nodes
+            p_dof += parent.back()->getPDoF(hasLeverChild);
+            r_dof += parent.back()->getRDoF();
+            
+            if(p_dof>=p_dof_req && r_dof>=r_dof_req || parents.back()->parent == NULL){
+                //We dont need anymore nodes to fuse data, or we are out of nodes
+                break;
+            } else {
+                //We need more nodes
+                parents.push_back(parents.back()->parent);
+            }
         }
+        return parents;
     }
 
-    void ArticulatedModel::fuseRotationMeasurement(const Measurement::Ptr& m, const Transform3D& toFusionSpace){
+    void Node::fusePositionMeasurement(const Measurement::Ptr& m, const Transform3D& toFusionSpace){
+        //Calculate error
+        if(m->isGlobal){
+            std::vector<Node::Ptr> fusion_chain = getRequiredParents(m);
+        }
+        //TODO: fuse chain
+    }
+
+    void Node::fuseRotationMeasurement(const Measurement::Ptr& m, const Transform3D& toFusionSpace){
 
     }
 
-    void ArticulatedModel::fuseRigidMeasurement(const Measurement::Ptr& m, const Transform3D& toFusionSpace){
+    void Node::fuseRigidMeasurement(const Measurement::Ptr& m, const Transform3D& toFusionSpace){
 
     }
 
-    void ArticulatedModel::fuseScaleMeasurement(const Measurement::Ptr& m, const Transform3D& toFusionSpace){
+    void Node::fuseScaleMeasurement(const Measurement::Ptr& m, const Transform3D& toFusionSpace){
 
     }
 
