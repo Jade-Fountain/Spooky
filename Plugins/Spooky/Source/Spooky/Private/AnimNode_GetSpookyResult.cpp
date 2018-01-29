0f2ebf1913bfced3799ef5f49c0ad3674daf40d9
Jake Fountain <jake.fountain@uon.edu.au>
2018-01-29 19:38:48 +1100

    Improved ik test model. Added printout for figuring out why the scale is small

--- Plugins/Spooky/Source/Spooky/Private/AnimNode_GetSpookyResult.cpp
+++ Plugins/Spooky/Source/Spooky/Private/AnimNode_GetSpookyResult.cpp
@@ -42,6 +42,7 @@
 				FString bone_name = Output.GetAnimBlueprint()->TargetSkeleton->GetReferenceSkeleton().GetBoneName(index).GetPlainNameString();
 				Output.Pose[FCompactPoseBoneIndex(index)] = spookyFP->getBoneTransform(bone_name);
 				//Output.Pose[FCompactPoseBoneIndex(index)].SetRotation(FQuat(0, 0, 0, 1));
+				SPOOKY_FLOG(Output.Pose[FCompactPoseBoneIndex(index)].ToString());
 			}
 		}
 
