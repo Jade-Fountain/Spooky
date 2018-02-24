9b6a8f775097fe83cf087e842149b84de8e3f60d
Jake Fountain <jake.fountain@uon.edu.au>
2018-02-24 14:40:08 +1100

    updated tests with new method signatures

--- Plugins/Spooky/Testing/CalibrationTests.cpp
+++ Plugins/Spooky/Testing/CalibrationTests.cpp
@@ -321,11 +321,13 @@ namespace FusionTesting
 			spooky::Core core;
 			spooky::Transform3D bonePose = spooky::Transform3D::Identity();
 			bonePose.translate(Eigen::Vector3f(1, 0, 0));
-			Eigen::Matrix3f c_var = Eigen::Matrix3f::Identity();
-			Eigen::Vector3f c_centre = Eigen::Vector3f::Zero();
-			core.addBoneNode(spooky::NodeDescriptor("bone1"), spooky::NodeDescriptor(""), bonePose, c_centre, c_var, 0.1);
-			core.addBoneNode(spooky::NodeDescriptor("bone2"), spooky::NodeDescriptor("bone1"), bonePose, c_centre, c_var, 0.1);
-			core.addBoneNode(spooky::NodeDescriptor("bone3"), spooky::NodeDescriptor("bone2"), bonePose, c_centre, c_var, 0.1);
+			bool modelVelocity = true;
+			int dim = modelVelocity ? 6 : 3;
+			Eigen::MatrixXf c_var = Eigen::MatrixXf::Identity(dim, dim);
+			Eigen::VectorXf c_centre = Eigen::VectorXf::Zero(dim);
+			core.addBoneNode(spooky::NodeDescriptor("bone1"), spooky::NodeDescriptor(""), bonePose, c_centre, c_var, 0.1,modelVelocity);
+			core.addBoneNode(spooky::NodeDescriptor("bone2"), spooky::NodeDescriptor("bone1"), bonePose, c_centre, c_var, 0.1, modelVelocity);
+			core.addBoneNode(spooky::NodeDescriptor("bone3"), spooky::NodeDescriptor("bone2"), bonePose, c_centre, c_var, 0.1, modelVelocity);
 			core.setReferenceSystem(spooky::NodeDescriptor("sys1"));
 			core.finaliseSetup();
 
