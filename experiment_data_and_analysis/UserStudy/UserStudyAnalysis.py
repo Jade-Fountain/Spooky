de75e83ae7655caceeb3dbc8eedd63b9ddee1ea1
Jake Fountain <jake.fountain@gmail.com>
2018-04-17 15:16:03 +1000

    Participant 3

--- experiment_data_and_analysis/UserStudy/UserStudyAnalysis.py
+++ experiment_data_and_analysis/UserStudy/UserStudyAnalysis.py
@@ -225,9 +225,9 @@ def getPValueNormGT0(data):
     pval = 1 - scipy.stats.norm.cdf(mean,scale=sigma/np.sqrt(data.shape[0]))    
     return pval
 
-plotThrowingData("Participant2")
+plotThrowingData("Participant3")
 
-participants = ["JakeTest_12_4_18","MattTest","MattTest2","Participant2"]
+participants = ["JakeTest_12_4_18","MattTest","MattTest2","Participant2","Participant3"]
 improvements, time_improvements, error_improvements = np.array([]),np.array([]),np.array([])
 
 first = True
