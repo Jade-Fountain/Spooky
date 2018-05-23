23a6db272dff6251a2c88082cc63853e21e5722b
Jake Fountain <jake.fountain@gmail.com>
2018-05-23 13:07:18 +1000

    Participant18

--- experiment_data_and_analysis/UserStudy/UserStudyAnalysis.py
+++ experiment_data_and_analysis/UserStudy/UserStudyAnalysis.py
@@ -608,7 +608,7 @@ saveFigure("UtilityResponses")
 
 
 def performanceAnalysis():
-    participants = [5,6,7,8,9,10,11,12,13,14,15,16,17]
+    participants = [5,6,7,8,9,10,11,12,13,14,15,16,17,18]
     # improvements, time_improvements, error_improvements = np.array([]),np.array([]),np.array([])
     # scores, times, errors = np.array([]),np.array([]),np.array([])
     parNames = []
@@ -655,9 +655,9 @@ def performanceAnalysis():
     saveFigure("AllThrows")
 
     #This participant
-    plotThrowingData(["Participant17"])
-    plt.title("Participant 17 Throws")    
-    saveFigure("Participant17Throws")
+    plotThrowingData(["Participant18"])
+    plt.title("Participant 18 Throws")    
+    saveFigure("Participant18Throws")
 
     #Improvements
     boxPlotColumns(improvements,deltaOrders)
